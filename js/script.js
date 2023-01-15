d5deeda2670de4ac5a07b1e58edeb83382d357aa
GreatBam <108512439+GreatBam@users.noreply.github.com>
2023-01-15 16:06:38 +0100

    Add class range

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ let hostNumberValue = document.getElementById("hostNumberValue");
 let calculateButton = document.getElementById("calculateButton");
 let neededBitSr = document.getElementById("neededBitSr");
 let neededBitIp = document.getElementById("neededBitIp");
+let classRange = document.getElementById("classRange");
 
 let maskTable = document.getElementById("showMaskTable").innerHTML;
 let ipTable = document.getElementById("showIpTable").innerHTML;
@@ -11,33 +12,52 @@ let maskTableContent;
 let ipTableContent;
 
 // functions
-calculateButton.onclick = function() {
-    console.log("subnetNumberValue : " + subnetNumberValue.value);
-    console.log("hostNumberValue : " + hostNumberValue.value);
-
+function subnetLog() {
     let subnetValue = subnetNumberValue.value;
-    let hostValue = hostNumberValue.value;
-
     let subnetReserve = (subnetValue * 0.1);
-    let hostReserve = (hostValue * 0.1);
-
-    console.log("totalSubnetNumber : " + subnetReserve);
-    console.log("totalHostNumber : " + hostReserve);
-
     let totalSubnetNumber = Math.ceil(parseFloat(subnetValue) + parseFloat(subnetReserve)).toFixed(1);
+    let subBitValue = Math.ceil((Math.log(totalSubnetNumber))/(Math.log(2)));
+    console.log("number of subnet : " + subnetNumberValue.value);
+    console.log("subnet reserve : " + subnetReserve);
+    console.log("total number of subnet : " + totalSubnetNumber);
+    return subBitValue;
+}
+
+function hostLog() {
+    let hostValue = hostNumberValue.value;
+    let hostReserve = (hostValue * 0.1);
     let totalHostNumber = Math.ceil(parseFloat(hostValue) + parseFloat(hostReserve)).toFixed(1);
-
-    console.log("totalSubnetNumber : " + totalSubnetNumber);
-    console.log("totalHostNumber : " + totalHostNumber);
-
-    let subnetBitNumber = Math.ceil((Math.log(totalSubnetNumber))/(Math.log(2)));
     let hostBitNumber = Math.ceil((Math.log(totalHostNumber+2))/(Math.log(2)));
+    console.log("number of host : " + hostNumberValue.value);
+    console.log("host reserve : " + hostReserve);
+    console.log("total number of host : " + totalHostNumber);
+    return hostBitNumber;
+}
 
-    console.log("subnetBitNumber : " + subnetBitNumber);
-    console.log("hostBitNumber : " + hostBitNumber);
+function classRangeFunc(value) {
+    let classRange = "";
+    if(value <= 8) {
+        classRange = "C";
+    } else if(value <= 16) {
+        classRange = "B";
+    } else if(value <= 24) {
+        classRange = "A";
+    }
+    return classRange;
+}
 
+calculateButton.onclick = function() {
+    let subnetBitNumber = subnetLog();
+    let hostBitNumber = hostLog();
+    let bitSum = subnetBitNumber + hostBitNumber;
+    console.log("total number of subnet bit : " + hostBitNumber);
+    console.log("total number of host bit : " + subnetBitNumber);
     neededBitSr.innerHTML = subnetBitNumber;
     neededBitIp.innerHTML = hostBitNumber;
+
+    let classRangeValue = classRangeFunc(bitSum);
+    classRange.innerHTML = classRangeValue;
+    classRange.style.color = "red";
 }
 
 // function showMaskTable() {
